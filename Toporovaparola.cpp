832e40a0c7c550917f7965cd214fe17ba7758cd0
SERGE GUEA <113424842+SERGE3-g@users.noreply.github.com>
2023-02-22 17:32:44 +0100

    topo trova la parola sono stanco mi dispiace

--- Toporovaparola.cpp
+++ Toporovaparola.cpp
@@ -19,17 +19,26 @@
 
 using namespace std;
 
+// provo un altro metodo che mi stampa tutto il testo
 int main()
 {
     ifstream fileInput("testi/maggio.txt");  // apertura file in lettura
-
+    if (fileInput.is_open()) {
+        string line;
+        while (getline(fileInput, line)) {
+            cout << line << endl;
+        }
+        fileInput.close();
+    }
     string riga;
     string parola ="immobile";
     string paragrafo="1";
 
     int numeroRigaParolaTrovata = 0;
 
-    while (getline(fileInput, riga)) {
+    // ho provato questo metodo che mi stampa il paragrafo della parola cercata
+
+    /*while (getline(fileInput, riga)) {
         numeroRigaParolaTrovata++;
         paragrafo += riga + "\n";
         cout << "Riga: " << numeroRigaParolaTrovata << " " << riga << endl;
@@ -45,5 +54,46 @@ int main()
         riga = paragrafo;
     }
     fileInput.close();
-    return 0;
+    return 0;*/
+
+    while(paragrafo!= "16")
+
+        paragrafo += riga + "\n";
+    cout << "Riga: " << numeroRigaParolaTrovata << " " << riga << endl;;
+
+    while (getline(fileInput, riga)) {
+        {
+            paragrafo += riga + "\n";
+            cout << "Riga: " << numeroRigaParolaTrovata << " " << riga << endl;
+            bool flag = false;                      // SE TROVO LA PAROLA IMPOSTO A TRUE
+            for (int i = 0; i < riga.length(); i++) // for tutta riga
+            {
+
+                for (int j = 0; j < parola.length(); j++) // for tutta parola
+                {
+
+                    if  (riga[i] == parola[j])// se le lettere sono uguali
+                    {
+                        if (j == parola.length() - 1) // se le lettere sono uguali E sono all'ultima lettera della parola
+                        {
+                            flag = true;
+                        }
+                        i++;
+                    }
+                    {
+                        break;
+                    }
+                }
+                if (flag)
+                    break;
+            }
+
+            if (flag)
+
+            {
+                cout << "PAROLA TROVATA IN RIGA: " << numeroRigaParolaTrovata << " - " << riga << endl;
+            }
+        }
+        fileInput.close();
+    }
 }
